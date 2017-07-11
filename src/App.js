eaa06eff101859ee2c8f1be0ee099040f953fd2c
Salmon Shaman <salmonshaman@salmons-imac.gateway.sonic.net>
2017-07-10 22:13:07 -0700

    hmm

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Moose />
+        <Moose fill="red" />
       </div>
     );
   }
--- src/Moose.js
+++ src/Moose.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 
 class Moose extends Component {
 
-	static myColor = { fill: "red" };
-
 	  render() {
-	  	var fill 
 	    return (
 	    	<div>
 				<svg height="100" width="100">
-				  <circle cx="50" cy="50" r="40" fill={myColor.props.fill} />
+				  <circle cx="50" cy="50" r="40" fill={this.props.fill} />
 				</svg>
 	    	</div>
 	    );
