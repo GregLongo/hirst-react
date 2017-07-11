25ebea01354ec51f2388f3c7e4b3bee67354692c
Salmon Shaman <salmonshaman@salmons-imac.gateway.sonic.net>
2017-07-10 22:51:35 -0700

    gridmaybe

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import Moose from './Moose';
 
 class App extends Component {
   render() {
-  	var dots= [];
-  	var dotsX = [];
+  	var dots= [][];
 	var randomColor;
   	
+	 for (var j=0 ; j < 8 ; j++){
 
 	  	for (var i=0 ; i < 8 ; i++){
 
 	  	        randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 
-	  	        dots[i] = <Moose fill={ randomColor } />;
+	  	        dots[i][j] = <Moose fill={ randomColor } />;
 				
 				}
-			
+			}
+
     return (
       <div>
       	{dots}
