44db559db9345cfc43ff3be43cb24146931c23b3
Salmon Shaman <salmonshaman@salmons-imac.gateway.sonic.net>
2017-07-10 22:48:07 -0700

    testing i

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Moose from './Moose';
 class App extends Component {
   render() {
   	var dots= [];
+  	var dotsX = [];
 	var randomColor;
+  	
 
-  	for (var i=0 ; i < 8 ; i++){
-  	        
-  	        randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+	  	for (var i=0 ; i < 8 ; i++){
 
-  	        dots.push(<Moose fill={ randomColor } />);
+	  	        randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+
+	  	        dots[i] = <Moose fill={ randomColor } />;
+				
+				}
 			
-			}
     return (
       <div>
       	{dots}
