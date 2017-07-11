cdaac23784b45cff95072888837feee0cbd5d27a
Salmon Shaman <salmonshaman@salmons-imac.gateway.sonic.net>
2017-07-10 21:51:14 -0700

    try

--- src/Moose.js
+++ src/Moose.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 class Moose extends Component {
 
 	static myColor = { fill: "red" };
-	
+
 	  render() {
+	  	var fill 
 	    return (
 	    	<div>
 				<svg height="100" width="100">
-				  <circle cx="50" cy="50" r="40" fill={this.props.fill} />
+				  <circle cx="50" cy="50" r="40" fill={myColor.props.fill} />
 				</svg>
 	    	</div>
 	    );
