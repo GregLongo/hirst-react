7e4e28fcc910808a35bcd00df18d1b5bc0eebfaa
Salmon Shaman <salmonshaman@salmons-imac.gateway.sonic.net>
2017-07-10 22:20:08 -0700

    rando maybe

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Moose from './Moose';
 
 class App extends Component {
   render() {
+  	var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
     return (
       <div>
-        <Moose fill="red" />
+        <Moose fill={ randomColor } />
       </div>
     );
   }
