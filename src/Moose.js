cf2b534355e2056b4f647ddf56fab2f778d8d1d8
Salmon Shaman <salmonshaman@salmons-imac.gateway.sonic.net>
2017-07-10 21:32:10 -0700

    happy little circle

--- src/Moose.js
+++ src/Moose.js
@@ -4,8 +4,9 @@ class Moose extends Component {
   render() {
     return (
     	<div>
-
-    	<h1> Mooooooooose</h1>
+			<svg height="100" width="100">
+			  <circle cx="50" cy="50" r="40" fill="lightgreen" />
+			</svg>
     	</div>
     );
   }
