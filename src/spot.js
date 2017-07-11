3f48d69953ce3f1adfd407f362bb487e04ed219c
Salmon Shaman <salmonshaman@salmons-imac.gateway.sonic.net>
2017-07-10 20:53:08 -0700

    one green spot

---
+++ src/spot.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Spot extends Component {
+  render() {
+    return (
+      <div className="Spot">
+        <svg height="100" width="100">
+		  <circle cx="50" cy="50" r="40"  fill="lightgreen" />
+		</svg>
+
+      </div>
+    );
+  }
+}
+
+export default Spot;
